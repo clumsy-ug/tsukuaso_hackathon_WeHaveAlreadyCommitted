518fd072b9689a30e40ccdeb927475e478320b2d
clumsy-ug <rutobe740@gmail.com>
2025-02-22 03:50:42 +0900

    Homeのコンポーネント分離

--- front/src/routes/__root.tsx
+++ front/src/routes/__root.tsx
@@ -1,20 +1,11 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import Home from './-components/Home'
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div>
-        <Link to="/">
-          Home
-        </Link>{' '}
-        <Link to="/about">
-          About
-        </Link>
-      </div>
-
-      <hr />
-
+      <Home />
       <Outlet />
       <TanStackRouterDevtools />
     </>
