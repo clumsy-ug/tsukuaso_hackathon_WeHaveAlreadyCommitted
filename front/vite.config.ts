6b5abc8b8ca90d31f9c4b3a027f20630d823a83c
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-02-22 02:53:49 +0900

    importする際root/srcを~で省略可能に

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -8,15 +8,17 @@ export default defineConfig({
 })
 */
 
-
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
-  ],
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src')
+    }
+  }
 })
