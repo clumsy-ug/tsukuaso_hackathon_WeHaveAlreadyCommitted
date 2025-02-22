6f0287ddc1638d32c8123d72e7f108db98210c7c
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-02-22 09:27:18 +0900

    セッション取得に失敗したときnullを返すように変更

--- clientSupabase/supabase/auth/getSession.ts
+++ clientSupabase/supabase/auth/getSession.ts
@@ -1,21 +1,21 @@
-import { supabase } from "../client.ts";
+import { supabase } from '../client.ts'
 
 export const getSession = async () => {
   try {
-    const { data, error } = await supabase.auth.getSession();
-    if (error) throw error;
+    const { data, error } = await supabase.auth.getSession()
+    if (error) throw error
 
     const {
       data: { user },
-      error: userError,
-    } = await supabase.auth.getUser();
-    if (userError) throw userError;
+      error: userError
+    } = await supabase.auth.getUser()
 
-    return user;
-  } catch (error) {
-    if (error instanceof Error) {
-      throw new Error(error.message);
+    if (userError) {
+      return null
     }
-    throw new Error("セッションの取得に失敗しました");
+
+    return user
+  } catch (error) {
+    return null
   }
-};
+}
