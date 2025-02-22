b90bc77feec83a0b7f5ad2edfcd8e41146f53c6e
clumsy-ug <rutobe740@gmail.com>
2025-02-22 10:47:47 +0900

    santa/check 分割

--- front/src/routes/santa/check/index.tsx
+++ front/src/routes/santa/check/index.tsx
@@ -1,57 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
-import {
-  saveSantaPass,
-  isExitPasswordCheck,
-  verifySantaPass
-} from '../../../../../clientSupabase/supabase/santaPass/santaPass'
-import { useEffect, useState } from 'react'
+import SantaCheck from "./-components/SantaCheck"
+import SantaCheckPending from './-components/SantaCheckPending'
+import SantaCheckError from './-components/SantaCheckError'
+
 export const Route = createFileRoute('/santa/check/')({
-  component: RouteComponent
+  component: SantaCheck,
+  pendingComponent: SantaCheckPending,
+  errorComponent: SantaCheckError
 })
-
-function RouteComponent() {
-  const [password, setPassword] = useState<number>(1111) //TODO null回避で初期値1111 バリデーションチェックで4桁のみにしたい
-
-  const handleSave = async () => {
-    try {
-      await saveSantaPass(password)
-      alert('パスワードを保存しました')
-    } catch (error) {
-      console.error('エラー:', error)
-    }
-  }
-
-  const handlePassCheck = async () => {
-    const isCheck = await verifySantaPass(password)
-    if (isCheck) {
-      alert('認証成功')
-    } else {
-      alert('認証失敗')
-    }
-  }
-
-  useEffect(() => {
-    const initCheck = async () => {
-      const isExitPassword: boolean = await isExitPasswordCheck()
-      if (isExitPassword) {
-        alert('パスワードが設定済みです')
-      } else {
-        alert('パスワードが未設定です')
-      }
-    }
-
-    initCheck()
-  }, [])
-
-  return (
-    <div>
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(Number(e.target.value))}
-      />
-      <button onClick={handleSave}>登録</button>
-      <button onClick={handlePassCheck}>認証</button>
-    </div>
-  )
-}
