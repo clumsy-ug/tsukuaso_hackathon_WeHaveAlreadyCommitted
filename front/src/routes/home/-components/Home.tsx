c395e2969f1376000a85a53f413127dc199a5be1
clumsy-ug <rutobe740@gmail.com>
2025-02-23 08:31:58 +0900

    名前変更

--- front/src/routes/home/-components/Home.tsx
+++ front/src/routes/home/-components/Home.tsx
@@ -1,14 +1,14 @@
 import { Suspense } from 'react'
 import { getSession } from '~/../../clientSupabase/supabase/auth/getSession'
 import HomePending from './HomePending'
-import PromiseReceiver from './PromiseReceiver'
+import ToSantaChat from './ToSantaChat'
 
 export default function Home() {
   const user = getSession()
 
   return (
     <Suspense fallback={<HomePending />}>
-      <PromiseReceiver user={user} />
+      <ToSantaChat user={user} />
     </Suspense>
   )
 }
