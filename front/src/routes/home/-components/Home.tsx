2288241e7f3b8643340b062bf1b4ffe80f8a2b3f
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-02-23 00:33:16 +0900

    /homeからLink to="/santa/check"へのpath修正

--- front/src/routes/home/-components/Home.tsx
+++ front/src/routes/home/-components/Home.tsx
@@ -1,11 +1,11 @@
-import { Link } from "@tanstack/react-router";
+import { Link } from '@tanstack/react-router'
 
 export default function Home() {
   return (
     <>
       <Link to="/child-chat">サンタさんと話す</Link>
       <br />
-      <Link to="/manage/check">管理者画面へ</Link>
+      <Link to="/santa/check">管理者画面へ</Link>
     </>
   )
 }
