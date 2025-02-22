9bcbf7ad868a7650b15e61128b97fc052b482d2b
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-02-23 01:31:23 +0900

    /home /child-chatのリンク削除

--- front/src/routes/home/-components/Home.tsx
+++ front/src/routes/home/-components/Home.tsx
@@ -3,8 +3,6 @@ import { Link } from '@tanstack/react-router'
 export default function Home() {
   return (
     <>
-      <Link to="/child-chat">サンタさんと話す</Link>
-      <br />
       <Link to="/santa/check">管理者画面へ</Link>
     </>
   )
