eccb433077acbaee8c0aa8ceed767e4f7161babc
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-02-23 00:11:41 +0900

    __rootレベルで認証が必要なページは認証チェック

--- front/src/routes/__root.tsx
+++ front/src/routes/__root.tsx
@@ -1,13 +1,30 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import NavbarTop from './-components/NavbarTop'
+import { getSession } from '~/../../clientSupabase/supabase/auth/getSession'
+import { redirect } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
+  beforeLoad: async ({ location }) => {
+    return checkAuthSession(location.pathname)
+  },
   component: () => (
     <>
       <NavbarTop />
       <Outlet />
       <TanStackRouterDevtools />
     </>
-  ),
+  )
 })
+
+const checkAuthSession = async (pathname: string) => {
+  const pulblicRoutes = ['/auth', '/', '/child-chat']
+  const session = await getSession()
+
+  if (session == null && !pulblicRoutes.includes(pathname)) {
+    throw redirect({
+      to: '/auth',
+      search: { redirect: pathname }
+    })
+  }
+}
