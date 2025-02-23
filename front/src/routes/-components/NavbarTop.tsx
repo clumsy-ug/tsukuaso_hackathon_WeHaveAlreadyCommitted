ba512a29b9764009e6d62eba4aeb56c56913c617
clumsy-ug <rutobe740@gmail.com>
2025-02-24 04:02:59 +0900

    Navbarスタイリング

--- front/src/routes/-components/NavbarTop.tsx
+++ front/src/routes/-components/NavbarTop.tsx
@@ -1,11 +1,26 @@
-import { Link } from "@tanstack/react-router";
+import { AppBar, Toolbar, IconButton } from '@mui/material';
+import { Link } from '@tanstack/react-router';
+import HomeIcon from '@mui/icons-material/Home';
 
 export default function NavbarTop() {
   return (
-    <>
-      <Link to="/">Home</Link>
-      {" "}
-      <br /><br />
-    </>
-  )
+    <AppBar position="sticky" sx={{ backgroundColor: '#f8bbd0' }}> {/* 薄いピンク */}
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          component={Link}
+          to="/"
+          aria-label="home"
+          sx={{
+            '&:hover': {
+              backgroundColor: '#f48fb1', // 濃いピンク (ホバー時)
+              color: 'white' // ホバー時のアイコンの色
+            },
+          }}
+        >
+          <HomeIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
 }
